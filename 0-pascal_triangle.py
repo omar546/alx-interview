4c5b799c66d182e2f9a93b784a5e6ecd0aeb3a24
Omar Abo El-Khair <71936776+omar546@users.noreply.github.com>
2024-04-29 01:06:50 +0300

    Create 0-pascal_triangle.py

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Pascal Triangle Interview Challenge"""
+
+def pascal_triangle(n):
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle if n > 0 else []
