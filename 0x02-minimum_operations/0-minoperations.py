d83d813b76a078459527dba4e5e2e49c9ba73bae
omar546 <omer.mkawy.om@gmail.com>
2024-05-09 17:32:56 +0300

    minOp

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,26 +2,27 @@
 '''minimum operations challenge.'''
 
 def minOperations(n):
-  """
-  Calculates the fewest operations needed to have n 'H' characters.
 
-  Args:
+    """
+    Calculates the fewest operations needed to have n 'H' characters.
+
+    Args:
       n: Target number of 'H' characters.
 
-  Returns:
+    Returns:
       Minimum number of operations needed to achieve n 'H' characters.
       Returns 0 if n is impossible to achieve.
-  """
-  if n == 0:
-    return 0
+    """
+    if n <= 1:
+        return 0
 
-  current_chars = 1
-  operations = 0
+    operations = 0
+    divisor = 2
 
-  while current_chars * 2 <= n:
-    current_chars *= 2
-    operations += 1  
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n /= divisor
+        divisor += 1
 
-  operations += (n - current_chars) // current_chars
-
-  return operations
+    return operations
